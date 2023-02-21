9973a1fc9c319ffafd0098a1b10322cb165051f3
cheickkins <cheickna378@gmail.com>
2023-02-21 23:05:02 +0000

    Création du modèle Order avec les dépendances nécessaires tenant compte de OrderDetails

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -16,25 +16,19 @@ class Order
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 64)]
+    #[ORM\Column(length: 255)]
     private ?string $reference = null;
+
+    #[ORM\Column]
+    private ?int $coupon_id = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $created_at = null;
 
-    #[ORM\ManyToOne(inversedBy: 'orders')]
-    private ?Coupon $coupon = null;
-
-    #[ORM\ManyToOne(inversedBy: 'orders')]
-    private ?Customer $customer = null;
-
-    #[ORM\OneToMany(mappedBy: 'order_', targetEntity: OrderDetails::class)]
+    #[ORM\OneToMany(mappedBy: 'order_id', targetEntity: OrderDetail::class)]
     private Collection $orderDetails;
 
-    #[ORM\OneToOne(mappedBy: 'order_', cascade: ['persist', 'remove'])]
-    private ?Payment $payment = null;
-
-    #[ORM\OneToMany(mappedBy: 'order_', targetEntity: Delivery::class)]
+    #[ORM\OneToMany(mappedBy: 'order_id', targetEntity: Delivery::class)]
     private Collection $deliveries;
 
     public function __construct()
@@ -60,6 +54,18 @@ class Order
         return $this;
     }
 
+    public function getCouponId(): ?int
+    {
+        return $this->coupon_id;
+    }
+
+    public function setCouponId(int $coupon_id): self
+    {
+        $this->coupon_id = $coupon_id;
+
+        return $this;
+    }
+
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->created_at;
@@ -72,73 +78,32 @@ class Order
         return $this;
     }
 
-    public function getCoupon(): ?Coupon
-    {
-        return $this->coupon;
-    }
-
-    public function setCoupon(?Coupon $coupon): self
-    {
-        $this->coupon = $coupon;
-
-        return $this;
-    }
-
-    public function getCustomer(): ?Customer
-    {
-        return $this->customer;
-    }
-
-    public function setCustomer(?Customer $customer): self
-    {
-        $this->customer = $customer;
-
-        return $this;
-    }
-
     /**
-     * @return Collection<int, OrderDetails>
+     * @return Collection<int, OrderDetail>
      */
     public function getOrderDetails(): Collection
     {
         return $this->orderDetails;
     }
 
-    public function addOrderDetail(OrderDetails $orderDetail): self
+    public function addOrderDetail(OrderDetail $orderDetail): self
     {
         if (!$this->orderDetails->contains($orderDetail)) {
             $this->orderDetails->add($orderDetail);
-            $orderDetail->setOrder($this);
+            $orderDetail->setOrderId($this);
         }
 
         return $this;
     }
 
-    public function removeOrderDetail(OrderDetails $orderDetail): self
+    public function removeOrderDetail(OrderDetail $orderDetail): self
     {
         if ($this->orderDetails->removeElement($orderDetail)) {
             // set the owning side to null (unless already changed)
-            if ($orderDetail->getOrder() === $this) {
-                $orderDetail->setOrder(null);
+            if ($orderDetail->getOrderId() === $this) {
+                $orderDetail->setOrderId(null);
             }
         }
-
-        return $this;
-    }
-
-    public function getPayment(): ?Payment
-    {
-        return $this->payment;
-    }
-
-    public function setPayment(Payment $payment): self
-    {
-        // set the owning side of the relation if necessary
-        if ($payment->getOrder() !== $this) {
-            $payment->setOrder($this);
-        }
-
-        $this->payment = $payment;
 
         return $this;
     }
@@ -155,7 +120,7 @@ class Order
     {
         if (!$this->deliveries->contains($delivery)) {
             $this->deliveries->add($delivery);
-            $delivery->setOrder($this);
+            $delivery->setOrderId($this);
         }
 
         return $this;
@@ -165,8 +130,8 @@ class Order
     {
         if ($this->deliveries->removeElement($delivery)) {
             // set the owning side to null (unless already changed)
-            if ($delivery->getOrder() === $this) {
-                $delivery->setOrder(null);
+            if ($delivery->getOrderId() === $this) {
+                $delivery->setOrderId(null);
             }
         }
 
